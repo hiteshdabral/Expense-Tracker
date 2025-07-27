0a6475c4a64a7a2ee4a2d813b392a9b50c28a9e6
hiteshdabral <hiteshdabral03@gmail.com>
2025-07-27 22:36:52 +0530

    Update environment configuration and database connection settings

--- expense-tracker-be/src/db/index.ts
+++ expense-tracker-be/src/db/index.ts
@@ -3,8 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl:isProduction? {
+    rejectUnauthorized: false,
+  }:false,
 });
 
 export default pool;
