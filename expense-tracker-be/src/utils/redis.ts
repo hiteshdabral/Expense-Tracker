5022a47770e51d58b1967444153d5ea9407de949
hiteshdabral <hiteshdabral03@gmail.com>
2025-07-27 22:53:26 +0530

    Refactor Redis client initialization to use a single URL configuration

--- expense-tracker-be/src/utils/redis.ts
+++ expense-tracker-be/src/utils/redis.ts
@@ -1,10 +1,13 @@
 import { Redis } from 'ioredis';
 
-const redisClient = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: Number(process.env.REDIS_PORT) || 6379,
-    password: process.env.REDIS_PASSWORD,
-});
+// const redisClient = new Redis({
+//     host: process.env.REDIS_HOST || 'localhost',
+//     port: Number(process.env.REDIS_PORT) || 6379,
+//     password: process.env.REDIS_PASSWORD,
+// });
+
+const redisClient = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
+
 
 redisClient.on('error', (err) => {
     console.error('Redis Client Error:', err);
